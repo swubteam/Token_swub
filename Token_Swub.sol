c2ad5411ef3adcd0fcfbe4901b91efbf4e01e4be
swubteam <swubteam@gmail.com>
2024-11-07 14:24:31 -0500

    Update Token_Swub.sol

--- Token_Swub.sol
+++ Token_Swub.sol
@@ -1,5 +1,9 @@
 // SPDX-License-Identifier: MIT
 // Toke of SWUB Exchange
+// pragma solidity ^0.8.18; opt 5000
+// address: 0xfd904DCa552E6eb931882982eFC3b41F2962b86a
+// Link: https://explorer.prismachain.io/address/0xfd904DCa552E6eb931882982eFC3b41F2962b86a/contracts#address-tabs
+
 pragma solidity ^0.8.18;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.6.0/contracts/token/ERC20/ERC20.sol";
