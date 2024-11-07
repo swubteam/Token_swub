d3f1bdc2a85d529941fc373a2f2497fdcf152c29
swubteam <swubteam@gmail.com>
2024-11-07 12:27:03 -0500

    Create Token_Swub.sol

---
+++ Token_Swub.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+// Toke of SWUB Exchange
+pragma solidity ^0.8.18;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.6.0/contracts/token/ERC20/ERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.6.0/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.6.0/contracts/access/Ownable.sol";
+
+/// @custom:security-contact swubteam@gmail.com
+contract Swub is ERC20, ERC20Burnable, Ownable {
+    constructor()
+        ERC20("Swub", "SWUB")
+    {
+        _mint(msg.sender, 20000000 * 10 ** decimals());
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+}
